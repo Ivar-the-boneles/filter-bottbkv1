0becd54e57c901bb413347f1fa714a7a7f5ff718
Ivar-the-boneles <92593566+Ivar-the-boneles@users.noreply.github.com>
2021-12-02 16:10:44 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -51,8 +51,8 @@ async def cb_handler(client, query):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/Nonofcomedychannel"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/Nonofcomedychannel")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="Ivar-the-boneles/Autofilterbotv1")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
