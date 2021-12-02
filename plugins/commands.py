0e887bedee07e260ff0eac8660678a5a63f50d3f
Ivar-the-boneles <92593566+Ivar-the-boneles@users.noreply.github.com>
2021-12-02 16:13:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,8 +247,8 @@ async def help(client, message):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/Nonofcomedychannel"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/Nonofcomedychannel")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="Ivar-the-boneles/Autofilterbotv1")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
